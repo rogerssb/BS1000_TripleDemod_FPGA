40885e5290dd9452aa87d715f868d3e02044a590
Larry Koos <Larry@koostech.com>
2019-07-29 23:42:18 +0000

    Testing to see what sim files are required to save in the repository

--- semcoDemod.sim/simLdpc/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2017.2\\bin
-call %xv_path%/xsim test_behav -key {Behavioral:simLdpc:Functional:test} -tclbatch test.tcl -view C:/modem/vivado2017/simLdpc.wcfg -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
